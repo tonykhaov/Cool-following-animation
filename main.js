c561c094ac55d6dfe6f50b57d70ebb34998c3cbe
Tony KHAOV <tony.khaov@gmail.com>
2020-04-27 20:42:56 +0200

    :rocket: Deploy fun animation go hover on it :) v2

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const links = document.querySelectorAll("a");
 const ul = document.querySelector("ul");
 const highlight = document.querySelector(".highlight");
+const firstLink = document.querySelector("a");
 
+let lastElement;
 function handleEnter() {
+  if (lastElement) {
+    lastElement.classList.remove("active");
+  } else {
+    firstLink.classList.remove("active");
+  }
+
   linkCoords = this.getBoundingClientRect();
   highlight.style.opacity = 1;
   highlight.style.width = `${linkCoords.width}px`;
   highlight.style.height = `${linkCoords.height}px`;
   highlight.style.bottom = `${linkCoords.top}`;
   highlight.style.left = `${linkCoords.left}`;
+
+  lastElement = this;
 }
 
 function handleLeave() {
   highlight.style.width = 0;
   highlight.style.opacity = 0;
+  lastElement.classList.add("active");
 }
 
 links.forEach((link) => link.addEventListener("mouseenter", handleEnter));
